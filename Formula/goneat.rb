8fe9ba10b5ef5d7a0278cfeff1c625f42800b1ea
Dave Thompson <dave.thompson@3leaps.net>
2025-12-12 14:35:54 -0500

    goneat: update to v0.3.15
    - Bump version from 0.3.14 to 0.3.15
    - Update SHA256 checksums for all platforms (darwin/linux amd64/arm64)
    
    Release: https://github.com/fulmenhq/goneat/releases/tag/v0.3.15

--- Formula/goneat.rb
+++ Formula/goneat.rb
@@ -1,30 +1,30 @@
 class Goneat < Formula
   desc "Go developer tool for neat code and smooth workflows"
   homepage "https://github.com/fulmenhq/goneat"
-  version "0.3.14"
+  version "0.3.15"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/fulmenhq/goneat/releases/download/v0.3.14/goneat_v0.3.14_darwin_amd64.tar.gz"
-      sha256 "25ed351c4733670548d6de5eb5d9d284565d45111dee0180787f9bbd7b5f0a9a"
+      url "https://github.com/fulmenhq/goneat/releases/download/v0.3.15/goneat_v0.3.15_darwin_amd64.tar.gz"
+      sha256 "83fdb039c2adc38e52253a98d474dccdb6f754a800a91d92f0cc7d6148a95870"
     end
 
     on_arm do
-      url "https://github.com/fulmenhq/goneat/releases/download/v0.3.14/goneat_v0.3.14_darwin_arm64.tar.gz"
-      sha256 "c5cbe99c559a3fb1c38b0b8a65780493131b64757db827be6786277d18e85620"
+      url "https://github.com/fulmenhq/goneat/releases/download/v0.3.15/goneat_v0.3.15_darwin_arm64.tar.gz"
+      sha256 "0ffa5840b55768f8ed89d6e325eb5c946ea28948aa9bba9a737a23c8e9e7749c"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/fulmenhq/goneat/releases/download/v0.3.14/goneat_v0.3.14_linux_amd64.tar.gz"
-      sha256 "b9c0020824a344a92179a5130b8ce3b7aed516c9bfa054a1fb809d41176e7349"
+      url "https://github.com/fulmenhq/goneat/releases/download/v0.3.15/goneat_v0.3.15_linux_amd64.tar.gz"
+      sha256 "d223555894b549f3070d5631575fc90e10bdfdb1030825bafe84df92e00a4f9f"
     end
 
     on_arm do
-      url "https://github.com/fulmenhq/goneat/releases/download/v0.3.14/goneat_v0.3.14_linux_arm64.tar.gz"
-      sha256 "07375fb31cb94b4821b7b3d08cb03529d1d1f12e4b40b371edd032ab56077442"
+      url "https://github.com/fulmenhq/goneat/releases/download/v0.3.15/goneat_v0.3.15_linux_arm64.tar.gz"
+      sha256 "547407d131b94c0971a96ba014aafcfd90cf3c91e73a45f73e90f585524378df"
     end
   end
 
