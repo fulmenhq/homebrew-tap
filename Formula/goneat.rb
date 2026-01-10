5513a5915199695b7320fc15091e6b5c2eb1feea
Dave Thompson <dave.thompson@3leaps.net>
2026-01-10 17:48:35 -0500

    Update goneat to v0.4.4

--- Formula/goneat.rb
+++ Formula/goneat.rb
@@ -1,30 +1,30 @@
 class Goneat < Formula
   desc "Go developer tool for neat code and smooth workflows"
   homepage "https://github.com/fulmenhq/goneat"
-  version "0.4.3"
+  version "0.4.4"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/fulmenhq/goneat/releases/download/v0.4.3/goneat_v0.4.3_darwin_amd64.tar.gz"
-      sha256 "ccb628a589310d8c8084bb41bc149b19b906839f8015b8967572c920b8763bd7"
+      url "https://github.com/fulmenhq/goneat/releases/download/v0.4.4/goneat_v0.4.4_darwin_amd64.tar.gz"
+      sha256 "f719ce7437dd6761e74707d78ccbb4b7233edcecc56d41e3c15d0d87c27b3be3"
     end
 
     on_arm do
-      url "https://github.com/fulmenhq/goneat/releases/download/v0.4.3/goneat_v0.4.3_darwin_arm64.tar.gz"
-      sha256 "d7f7ad75ce4c839ff45b8f61ad4088f8b43613cf7951430e4d5e46b7a8a7a0b0"
+      url "https://github.com/fulmenhq/goneat/releases/download/v0.4.4/goneat_v0.4.4_darwin_arm64.tar.gz"
+      sha256 "33eee660545f744e0c2e163da142c804bf5dafe25c4b4fd7cf46e86a6f4506d6"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/fulmenhq/goneat/releases/download/v0.4.3/goneat_v0.4.3_linux_amd64.tar.gz"
-      sha256 "2861b7ed78fd067e7085e9373340bb4300c8b204cc1e10820cc1652927d788b5"
+      url "https://github.com/fulmenhq/goneat/releases/download/v0.4.4/goneat_v0.4.4_linux_amd64.tar.gz"
+      sha256 "1cdd502f42e28c1125d39ffa041d9c9e6fb5560e047c82983b704f0622f88062"
     end
 
     on_arm do
-      url "https://github.com/fulmenhq/goneat/releases/download/v0.4.3/goneat_v0.4.3_linux_arm64.tar.gz"
-      sha256 "863901e8efa2e3fd913d3b39a7294835e534f0307980074adf91d1e957d4e93d"
+      url "https://github.com/fulmenhq/goneat/releases/download/v0.4.4/goneat_v0.4.4_linux_arm64.tar.gz"
+      sha256 "113ad6d8f71edc824c6aa12fd174766b619be4f3fb79d740214800a077753f7d"
     end
   end
 
