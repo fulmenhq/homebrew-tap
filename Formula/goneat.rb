f742d3bc3c7640e68a0d215bb32835f4021ba4b8
Dave Thompson <dave.thompson@3leaps.net>
2026-01-17 17:57:45 -0500

    goneat 0.5.0
    Vulnerability scanning & TypeScript typecheck release.
    
    See https://github.com/fulmenhq/goneat/releases/tag/v0.5.0

--- Formula/goneat.rb
+++ Formula/goneat.rb
@@ -1,30 +1,30 @@
 class Goneat < Formula
   desc "Go developer tool for neat code and smooth workflows"
   homepage "https://github.com/fulmenhq/goneat"
-  version "0.4.5"
+  version "0.5.0"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/fulmenhq/goneat/releases/download/v0.4.5/goneat_v0.4.5_darwin_amd64.tar.gz"
-      sha256 "8db053c6d3fa261788110be9f86311fac057538335f9f8fdb0b294c399010e81"
+      url "https://github.com/fulmenhq/goneat/releases/download/v0.5.0/goneat_v0.5.0_darwin_amd64.tar.gz"
+      sha256 "295259db2bfde77067dd04d93356f5b60df00233b39545c7c34bcf5f47858731"
     end
 
     on_arm do
-      url "https://github.com/fulmenhq/goneat/releases/download/v0.4.5/goneat_v0.4.5_darwin_arm64.tar.gz"
-      sha256 "1b31588864ab32fcb9f4964132919af216eae26f3d3668af77eff1dd67fe3471"
+      url "https://github.com/fulmenhq/goneat/releases/download/v0.5.0/goneat_v0.5.0_darwin_arm64.tar.gz"
+      sha256 "0497f6e0958a295b01ce53b332acbdc23dacb61a9a37915f22ea685518bdb4c2"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/fulmenhq/goneat/releases/download/v0.4.5/goneat_v0.4.5_linux_amd64.tar.gz"
-      sha256 "a023a76da95ac4e3c25a62f756b5d5d1f20c30ac76ccdccf023524461fcbb8cc"
+      url "https://github.com/fulmenhq/goneat/releases/download/v0.5.0/goneat_v0.5.0_linux_amd64.tar.gz"
+      sha256 "6a32e9dbb2595b9765888053a7c57562f3c56e54cc537886a867cba2e8b9b81c"
     end
 
     on_arm do
-      url "https://github.com/fulmenhq/goneat/releases/download/v0.4.5/goneat_v0.4.5_linux_arm64.tar.gz"
-      sha256 "a6d5e38dc3bd1f0172f868239ce2804dbcdcaca02b7d50725b534ffcd80b7346"
+      url "https://github.com/fulmenhq/goneat/releases/download/v0.5.0/goneat_v0.5.0_linux_arm64.tar.gz"
+      sha256 "bac34200ba03d2b90d8dde9a6d512d8e20d1e7df7c7c6473f10e8b62e2f7c6ec"
     end
   end
 
