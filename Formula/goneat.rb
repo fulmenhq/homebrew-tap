566194e87ade822fa8234ee88a9fe9590fc06d83
Dave Thompson <64275573+3leapsdave@users.noreply.github.com>
2026-01-23 09:34:39 -0500

    goneat: update to v0.5.2

--- Formula/goneat.rb
+++ Formula/goneat.rb
@@ -1,30 +1,30 @@
 class Goneat < Formula
   desc "Go developer tool for neat code and smooth workflows"
   homepage "https://github.com/fulmenhq/goneat"
-  version "0.5.1"
+  version "0.5.2"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/fulmenhq/goneat/releases/download/v0.5.1/goneat_v0.5.1_darwin_amd64.tar.gz"
-      sha256 "fa16f1c9d77e1c8ebe6baa4ec138ff520bba47f1104b2c853da632cf3d694a46"
+      url "https://github.com/fulmenhq/goneat/releases/download/v0.5.2/goneat_v0.5.2_darwin_amd64.tar.gz"
+      sha256 "ee06fb5ae4bbe052d576eed49b4ea338742bce767958ca24d5a3fdbf273352a0"
     end
 
     on_arm do
-      url "https://github.com/fulmenhq/goneat/releases/download/v0.5.1/goneat_v0.5.1_darwin_arm64.tar.gz"
-      sha256 "9c0d203871822321a6f8afea82b4b9e5b94dd776857d7ca3b8d5f566d407ff1e"
+      url "https://github.com/fulmenhq/goneat/releases/download/v0.5.2/goneat_v0.5.2_darwin_arm64.tar.gz"
+      sha256 "5974f5c9dc403a4e6f1ec6e7bd74350c165a4bc55c138fd682ba829a4fccdec8"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/fulmenhq/goneat/releases/download/v0.5.1/goneat_v0.5.1_linux_amd64.tar.gz"
-      sha256 "8ba31f7a00ac07af5e324c961c7027a396c9d488a5da8ace9df340f3aef28225"
+      url "https://github.com/fulmenhq/goneat/releases/download/v0.5.2/goneat_v0.5.2_linux_amd64.tar.gz"
+      sha256 "2738de369e2b69265fbfdf62a89c9f2958175495e57dc6f6bd7b314c497c6f55"
     end
 
     on_arm do
-      url "https://github.com/fulmenhq/goneat/releases/download/v0.5.1/goneat_v0.5.1_linux_arm64.tar.gz"
-      sha256 "2ab1ac96942b228fbc057ab94d4cd89bbb6b0f31cdf7a7078055dbe6fd75afc4"
+      url "https://github.com/fulmenhq/goneat/releases/download/v0.5.2/goneat_v0.5.2_linux_arm64.tar.gz"
+      sha256 "e73357a42cfe134d4332bd68ec2f22c70f9b15c02700a2fbf597a0d33e1e5c64"
     end
   end
 
